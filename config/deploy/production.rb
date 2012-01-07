b228e02054381c5d16efc0095676c27f8627110d
azaupa <azaupa@TheWhiteOne.local>
2012-01-07 22:26:02 +0100

    capify

---
+++ config/deploy/production.rb
@@ -0,0 +1,9 @@
+puts '***DEPLOY TO PRODUCTION SERVER***'
+
+puts '*** Deploying to: PRODUCTION environment ***'
+
+server "jupiter.azaupa.info", :app, :web, :db, :primary => true
+
+set :rails_env, "production"
+set :branch, "master"
+set :deploy_to, "/home/web/apps/production/#{application}"
